8612804cc10cced579fb926260799f71e80470e2
Yasemin <63241997+Svartalfr@users.noreply.github.com>
2020-10-23 18:52:57 +0300

    arduino trafik lambası kodu

---
+++ trafik_lambas_.ino
@@ -0,0 +1,26 @@
+void setup() {
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(5,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(2,HIGH);
+  delay(10000);
+  digitalWrite(5,HIGH);
+  delay(2000);
+  digitalWrite(2,LOW);
+  digitalWrite(3,LOW);
+  digitalWrite,(5,HIGH);
+  delay(5000);
+  digitalWrite(5,LOW);
+  digitalWrite(3,HIGH);
+  delay(2000);
+  digitalWrite(3,LOW);
+  
+
+  
+
+
+  
+}
